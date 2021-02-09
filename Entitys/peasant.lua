23a85d1fdbd94bd146a22320dbb5c7dc2d1b0ae6
K <keithmaccuish5@gmail.com>
2021-02-09 07:41:59 -0700

    if the entity the player is acting on has a want create a popup.

--- Entitys/peasant.lua
+++ Entitys/peasant.lua
@@ -1,17 +1,27 @@
 function updatePeasant(peasant)
+
   for i = 1, table.maxn(peasant.wants) do
-    if peasant.wants[i] ~= nil then
+
+    if peasant.wants[i] ~= nil and table.maxn(peasant.wants) > 0 then
       itemName = peasant.wants[i].itemName
       item = getItemByName(itemName)
-      for i = 1, table.maxn(peasant) do
-      end
-      if item.ingredients ~= nil then
 
-        if canCraft(peasant.inventory, item.ingredients,true) then
-          print("can craft")
-          table.remove(peasant.wants,i)
+      if hasItem(peasant.inventory,peasant.wants[i].itemName) then
+        peasant.wants[i] = nil
+      else
+
+        if item.ingredients ~= nil then
+          if canCraft(peasant.inventory, item.ingredients,true) then
+            print("can craft")
+            table.remove(peasant.wants,i)
+          end
         end
+
       end
+
+
+
+
     end
   end
 
