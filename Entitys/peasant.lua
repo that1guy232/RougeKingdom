7b63e74e11abe55ab34d539266d5b9e7985a8f10
K <keithmaccuish5@gmail.com>
2021-02-08 20:57:44 -0700

    Peasant crafts if it has the ingreadents.

--- Entitys/peasant.lua
+++ Entitys/peasant.lua
@@ -1,13 +1,16 @@
 function updatePeasant(peasant)
   for i = 1, table.maxn(peasant.wants) do
+    if peasant.wants[i] ~= nil then
+      itemName = peasant.wants[i].itemName
+      item = getItemByName(itemName)
+      for i = 1, table.maxn(peasant) do
+      end
+      if item.ingredients ~= nil then
 
-    itemName = peasant.wants[i].itemName
-    item = getItemByName(itemName)
-    if item.ingredients ~= nil then
-      print("has stuff")
-
-      if canCraft(peasant.inventory, item.ingredients,true) then
-        print("can craft")
+        if canCraft(peasant.inventory, item.ingredients,true) then
+          print("can craft")
+          table.remove(peasant.wants,i)
+        end
       end
     end
   end
